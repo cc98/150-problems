972a7cc2dde68b20a8053dcb778b2f3c79f170ff
cc98 <goby@foxmail.com>
2013-09-04 21:31:43 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,5 +1,17 @@
-char* str_reserve(char *str, int len){
-  assert(len);
+int str_len(char* str){
+  int i = 0;
+  assert(str);
+  while(*str) {i++;str++;}
+  return i;
+}
+
+char* str_reserve(char *str){
+  int len = str_len(str);
+  int i = 0; 
   char* result = (char*)malloc(sizeof(char) * len);
+  while(i <= (len - 1) / 2){
+    *(result + i) = *(str + len - i - 1);
+  }
   
+  return result;
 }
