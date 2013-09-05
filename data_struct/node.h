889c82cf3a751b62008d9c0583875e817b7f9b38
cc98 <goby@foxmail.com>
2013-09-05 16:42:44 +0800

    Update node.h

--- data_struct/node.h
+++ data_struct/node.h
@@ -1,12 +1,12 @@
 class Node{
 public:
-  Node next = 0;
+  Node* next = 0;
   int data;
   Node(int d):data(d){}
   void append(int d){
-    Node end = new Node(d);
-    Node n = this;
-    while(n.next != 0) n = n.next;
-    n.next = end;
+    Node* end = new Node(d);
+    Node* n = this;
+    while(n->next != 0) n = n->next;
+    n->next = end;
   }
 };
