48c93acea07178afecee29f13ac6799a286dad2d
cc98 <goby@foxmail.com>
2013-09-07 19:09:12 +0800

    Create 04_02.cpp

---
+++ 04_02.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include "data_struct/graph.h"
+
+enum VertexState{ IsVisited, UnVisited  };
+
+bool find_route(Graph *graph, Vertex* start, Vertex* end) {
+  std::vector<VertexState> isVisited
+}
