e599186cbe0c7659e51e8b893d29ea91b61ec9c7
cc98 <goby@foxmail.com>
2013-09-11 16:04:22 +0800

    Update string.h
    fixed index bug

--- algorithm/string.h
+++ algorithm/string.h
@@ -12,14 +12,15 @@ int algo_string_min(int x, int y, int z) {
 
 int levenshtein_distance(char* s, int len_s, char *t, int len_t) {
   int i, j;
-  int *d = (int *)malloc(sizeof(int) * len_s * len_t);
-  memset(d, 0, len_s * len_t * sizeof(int));
+  int *d = (int *)malloc(sizeof(int) * (len_s + 1) * (len_t + 1));
+  memset(d, 0, (len_s + 1) * (len_t + 1) * sizeof(int));
   for(i = 0; i < len_s; i++) d[i * len_t] = i;
   for(j = 0; j < len_t; j++) d[j] = j;
   
-  for(j = 0; j < len_t; j++)
-    for(i = 0; i < len_s; i++)
-      if(s[i] == t[j])
+  /* index of string from 1 to strlen*/
+  for(j = 1; j <= len_t; j++)
+    for(i = 1; i <= len_s; i++)
+      if(s[i - 1] == t[j - 1])
         d[i * len_s + j] = d[(i - 1) * len_s + (j - 1)];
       else
         d[i * len_s + j] = algo_string_min(
